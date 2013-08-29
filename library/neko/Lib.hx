93e9f52c89221c8ec066f43a0df96ef1979cf347
Yaroslav <Yaroslav@localhost>
2013-08-29 12:36:41 +0400

    * minor

--- library/neko/Lib.hx
+++ library/neko/Lib.hx
@@ -1,26 +1,23 @@
 /*
- * Copyright (c) 2005, The haXe Project Contributors
- * All rights reserved.
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * Copyright (C)2005-2012 Haxe Foundation
  *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
  *
- * THIS SOFTWARE IS PROVIDED BY THE HAXE PROJECT CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE HAXE PROJECT CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
- * DAMAGE.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
  */
 package neko;
 
@@ -75,15 +72,15 @@ class Lib {
 		Serialize using native Neko serialization. This will return a Binary string that can be
 		stored for long term usage. The serialized data is optimized for speed and not for size.
 	**/
-	public static function serialize( v : Dynamic ) : String {
-		return new String(__serialize(v));
+	public static function serialize( v : Dynamic ) : haxe.io.Bytes {
+		return haxe.io.Bytes.ofData(__serialize(v));
 	}
 
 	/**
 		Unserialize a string using native Neko serialization. See [serialize].
 	**/
-	public static function unserialize( s : String ) : Dynamic {
-		return untyped __unserialize(s.__s,__dollar__loader);
+	public static function unserialize( s : haxe.io.Bytes ) : Dynamic {
+		return untyped __unserialize(s.getData(),__dollar__loader);
 	}
 
 	/**
@@ -93,8 +90,8 @@ class Lib {
 		some data into mod_neko that was serialized on a different server using a different
 		file path.
 	**/
-	public static function localUnserialize( s : String ) : Dynamic {
-		return untyped __unserialize(s.__s,{
+	public static function localUnserialize( s : haxe.io.Bytes ) : Dynamic {
+		return untyped __unserialize(s.getData(),{
 			loadmodule : function(m,l) { return __dollar__exports; },
 			loadprim : function(p,n) { return __dollar__loader.loadprim(p,n); }
 		});
